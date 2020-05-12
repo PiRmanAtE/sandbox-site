6e6daaa94a26c29b4a0d035ad97897c8b0e3a15d
Александр Маляревский <aleksandrmalarevskij@MacBook-Pro-Aleksandr.local>
2020-05-12 15:54:37 +0300

    all data

---
+++ index.js
@@ -0,0 +1,33 @@
+import * as jquery from "js/jquery.min.js";
+import * as bootstrap from "js/bootstrap.min.js";
+import * as smoothscroll from "js/smoothscroll.js";
+import * as wow from "js/wow.min.js";
+import * as imagesloaded from "js/imagesloaded.pkgd.js";
+import * as isotope from "js/isotope.pkgd.min.js";
+import * as magnificPopup from "js/jquery.magnific-popup.min.js";
+import * as owlCarousel from "js/owl.carousel.min.js";
+import * as validate from "js/jquery.validate.min.js";
+import * as stallar from "js/jquery.stellar.min.js";
+import * as easypiechart from "js/jquery.easypiechart.min.js";
+import * as interface from "js/interface.js";
+import * as googleapis from "https://maps.googleapis.com/maps/api/js?v=3.exp.js";
+import * as gmap from "js/gmap.js";
+import * as utils from "js/utils.js";
+
+export default {
+  jquery,
+  bootstrap,
+  smoothscroll,
+  wow,
+  imagesloaded,
+  isotope,
+  magnificPopup,
+  owlCarousel,
+  validate,
+  stallar,
+  easypiechart,
+  interface,
+  googleapis,
+  gmap,
+  utils
+};
